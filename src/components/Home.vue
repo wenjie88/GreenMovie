caac588e7d152c46395fc3b999bc7e31b2decaae
wenjie88 <350668770@qq.com>
2017-05-15 18:03:56 +0800

    更新

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,32 @@
 <template>
-  <div> <h1>Home</h1> </div>
+  <md-card>
+    <md-card-media>
+      <img src="../assets/image.jpg" alt="People">
+    </md-card-media>
+  
+    <md-card-header>
+      <div class="md-title">这里是标题</div>
+      <div class="md-subhead">这里是子标题</div>
+    </md-card-header>
+  
+    <md-card-actions>
+      <md-button>动作</md-button>
+      <md-button>动作</md-button>
+    </md-card-actions>
+  
+    <md-card-content>
+      人心就像一个容器，装的快乐多了，烦恼自然就少；装的简单多了，纠结自然就少；装的满足多了，痛苦自然就少；装的理解多了，矛盾自然就少；装的宽容多了，仇恨自然就少。胸襟决定器量，境界决定高下。活着就是一种修行，修行就是修心。修得一颗平常心，无时不是快乐；修得一颗满足心，无处不是幸福。
+    </md-card-content>
+  </md-card>
 </template>
 
-<script>
-export default {
 
+<script>
+
+export default {
+  created(){
+    console.log(MdCard)
+  }
 }
 </script>
 
